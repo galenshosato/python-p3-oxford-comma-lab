d234e674f72403ccd3ab47f73bb01016459a6543
Galen Sho Sato <galen.sato@gmail.com>
2023-02-24 15:54:30 -0500

    Completed Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        short_list = " and ".join(items)
+        return short_list
+    else:
+        last_item = items.pop()
+        new_items = ", ".join(items)
+        new_list = new_items + f", and {last_item}"
+        return new_list
